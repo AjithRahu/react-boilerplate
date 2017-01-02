04532e2d3474db88ba9d8cabf868962d18b16b96
Ajith <ajith.rahu@gmail.com>
2017-01-01 22:12:57 -0500

    Updated Boilerplate

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <p>Hello!</p>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
